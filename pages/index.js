ba709cd7b65fc6ed22c9e4230906d6a96045e3f2
KellyMenzul <kmenzul@my.bcit.ca>
2021-05-19 00:58:25 -0700

    index

--- pages/index.js
+++ pages/index.js
@@ -1,97 +1,50 @@
 import Head from 'next/head';
 import styled from 'styled-components';
-import Menu from '../comps/Menu';
-import Didyouknow from '../comps/Didyouknow';
 import Start from '../comps/Start';
 import Tutorial from '../comps/Tutorial';
 import TutorialInfo from '../comps/TutorialInfo';
-import RegionButton from '../comps/RegionButton';
-import ImgText from '../comps/ImgText';
 import Header from '../comps/Header';
-import ProgramLogo from '../comps/ProgramLogo';
-import StudyOthers from '../comps/StudyOthers';
-import Bodytext from '../comps/Bodytext';
-import Flags from '../comps/Flags';
-import Marinequiz from '../comps/Marinequiz';
-import BottomBar from '../comps/BottomBar';
-import MovingImage from '../comps/MovingImage';
-import Medal from '../comps/Medal';
-import Progress from '../comps/Progress';
-import Why from '../comps/Why';
-
 
 const Holder = styled.div `
-.container1 {
-  display:flex;
-  flex-direction: column;
-  justify-content:center;
-  align-items:center;
-  margin-bottom: 100px;
+.container {
+    display:flex;
+    flex-direction: column;
+    justify-content:center;
+    align-items:center;
+};
+
+.top{
+    height:23rem;
+    min-width: 25rem;
 }
 
-`
+.top img{
+    width:15rem;
+}
 
-export default function Home() {
-  return <Holder>
-      <div className="container1">
-      <Progress />
-    </div>
-    <div className="container1">
-      <Why text="Aluminum cans end up in landfills which becomes food for animals"/>
-    </div>
-    <div className="container">
-    <Menu/>
-    </div>
-    <div className="container1">
-      <Didyouknow />
-    </div>
-    <div className="container1">
-      <Start /> 
-    </div>    
-    <div className="container1">
-      <Tutorial /> 
-    </div>
-    <div className="container1">
-      <TutorialInfo /> 
-    </div>
-    <div className="container1">
-      <RegionButton text="Marine" />
-    </div>
-    <div className="container1">
-      <ImgText />
-    </div>
-    <div className="container1">
-      <MovingImage />
-    </div>
-    <div className="container1">
-      <Header />
-    </div>
-    <div className="container1">
-      <ProgramLogo />
-    </div>
-    <div className="container1">
-      <StudyOthers />
-    </div>
-    <div className="container1">
-      <Bodytext />
-    </div>
-    <div className="container1">
-      <Medal/>
-    </div>
-    <div className="container1">
-      <Flags />
-    </div>
-    <div className="container1">
-      <Marinequiz />
-    </div>    
-    <div className="container1">
-      <BottomBar />
-    </div>
 
-  
-    
-    
-  
-  </Holder>
+.bottom {
+    height: 35rem;
+    background-color: #5769DF;
+    border-radius: 2rem 2rem 0rem 0rem;
+    min-width: 25rem;
+}
 
+`;
+
+export default function Regions() {
+    return <Holder>
+        <div className="top container">
+            <img src="/Logo.png"></img>
+        </div>
+        <div className="bottom container">
+            <Header text="Welcome!" weight="regular" color="white" fontfamily="Fredoka One" fontsize="30px"/>
+            <Header text="Let's start learning" weight="regular" color="white" fontfamily="Fredoka One" fontsize="30px"/>
+            <Start routeTo="/regions" fontfamily="'Fredoka One', cursive"/>
+            <Tutorial routeTo="/Tutorial"/>
+            <TutorialInfo/>
+        </div>
+        
+
+    </Holder>
 }
